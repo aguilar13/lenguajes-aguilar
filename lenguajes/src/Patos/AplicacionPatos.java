5fde14b26aca418142ab6b1264c1696ba5b7941b
T-102 <T-102@PC190>
2017-02-09 19:35:09 -0600

    polimorfismo

--- lenguajes/src/Patos/AplicacionPatos.java
+++ lenguajes/src/Patos/AplicacionPatos.java
@@ -11,21 +11,50 @@ package Patos;
  */
 public class AplicacionPatos {
     public static void main(String[] args){
-        PatoCanadiense pc1=new PatoCanadiense();
-        PatoCanadiense pc2=new PatoCanadiense();
-        System.out.println(PatoCanadiense.color);
-        pc1.comer();
-        PatoEuropeo pe1=new PatoEuropeo();
-    PatoEuropeo pe2=new PatoEuropeo();
-    System.out.println(PatoEuropeo.color);
-    PatoMexicano pm1=new PatoMexicano();
-    PatoMexicano pm2=new PatoMexicano();
-        System.out.println(PatoMexicano.color);
-
+        int valores[]={5,-2,5};
+        System.out.println(valores[2]);
+         int []dos={23,-8};
+         System.out.println(dos[1]);
+         int tres[]=new int[3];
+         tres[1]=-100;
+         tres[0]=4;
+         tres[2]=9;
+         System.out.println(tres[1]);
+         
+         for(int i=0;i<3;i++){
+             System.out.println(tres[i]);
+         }
+       for(int i: tres){
+           System.out.println(i);
+       }
+        String saludo[]={"hola,como,estas"};
+        for(String x:saludo){
+            System.out.println(x);
+            
+       ComportamientoVolar patos[]=new ComportamientoVolar[5];
+       patos[0]=new PatoMexicano();
+       patos[1]=new PatoMexicano();
+       patos[2]=new PatoEuropeo();
+       patos[3]=new PatoEuropeo();      
+       patos[4]=new PatoCanadiense();
+       
+       for(ComportamientoVolar p:patos){
+           p.volar();
+           
+            
+            
+            
+            
+        }  
+            
+            
+            
+        }
+    }
   
     
     }
          
      
     
-}
+
