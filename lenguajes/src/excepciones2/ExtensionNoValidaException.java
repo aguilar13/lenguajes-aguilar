1670bc2670b1b9a12588e50b48558f69a5d09cb5
T-102 <T-102@PC190>
2017-02-20 19:45:48 -0600

    extension3 validamos q sea un archivo txt

---
+++ lenguajes/src/excepciones2/ExtensionNoValidaException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones2;
+
+/**
+ *
+ * @author T-102
+ */
+public class ExtensionNoValidaException extends Exception{
+
+public ExtensionNoValidaException(){
+    super("lo siento pero solo leo archivos txt");
+}    
+}
---
+++ lenguajes/src/excepciones2/ValidacionArchivo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones2;
+
+/**
+ *
+ * @author T-102
+ */
+import java.io.*;
+
+public class ValidacionArchivo {
+    public static void validarExtencion(File file)throws ExtensionNoValidaException{
+        
+       // throw new ExtensionNoValidaException();
+        String nombre = file.getName();
+        
+       //como obtener la extension
+       int indice1 =nombre.indexOf(".");
+       int indice2 =nombre.length();
+       String extencion= nombre.substring(indice1-1,indice2);
+       if(!extencion.equals("txt"))
+           throw new ExtensionNoValidaException();
+    }
+    
+}
